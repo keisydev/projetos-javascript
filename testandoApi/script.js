ff5f8660f19aec719051030b57efb73a5f9ffc40
Keisy Rathlef <keisy.dev@gmail.com>
2025-04-24 15:56:35 +0100

    Filtrando informações

--- testandoApi/script.js
+++ testandoApi/script.js
@@ -13,9 +13,21 @@ const fetchApi = (value) => {
     return result 
 }
 
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode']
+
+const buildResult = (result) => {
+    const newObject = {}
+    keys.map((key) => document.getElementById(key))
+    .map((element) => {
+        element.checked && (newObject[element.name] = result[element.name])
+    })
+    return newObject
+}
+
 btnGo.addEventListener('click', async (event) => {
     event.preventDefault()
     const result = await fetchApi(characterId.value)
-    content.textContent = `${JSON.stringify(result, undefined, 2)}` 
+    //content.textContent = `${JSON.stringify(result, undefined, 2)}` 
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
     image.src = `${result.image}`
 })
