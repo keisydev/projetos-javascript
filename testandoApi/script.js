f737edb9ad77c056ecd433440f72a4a3b7897b0f
Keisy Rathlef <keisy.dev@gmail.com>
2025-04-24 16:51:34 +0100

    Atualização do html e do JS
    Atualização realizada para dar continuidade no desenvolvimento da página

--- testandoApi/script.js
+++ testandoApi/script.js
@@ -1,19 +1,20 @@
 const characterId = document.getElementById('characterId')
 const btnGo = document.getElementById('btn-go')
+const btnReset = document.getElementById('btn-reset')
 const content = document.getElementById('content')
+const containerResult = document.getElementById('result-style')
 const image = document.getElementById('img')
 
 const fetchApi = (value) => {
     const result = fetch (`https://rickandmortyapi.com/api/character/${value}`)
     .then((res) => res.json())
     .then((data) => {
-        console.log(data)
         return data 
     })
     return result 
 }
 
-const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode']
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode']
 
 const buildResult = (result) => {
     const newObject = {}
@@ -27,7 +28,6 @@ const buildResult = (result) => {
 btnGo.addEventListener('click', async (event) => {
     event.preventDefault()
     const result = await fetchApi(characterId.value)
-    //content.textContent = `${JSON.stringify(result, undefined, 2)}` 
     content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
     image.src = `${result.image}`
 })
